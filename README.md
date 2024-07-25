fe8db97bdada3da5697a4f0d95e44f53759d565f
joecre <Joecrea@protonmail.com>
2024-07-25 18:28:38 -0400

    removed my_env, added pipenv, added readme WIP, working on fix for numpy na error

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# carapp
-""" This application explores different variables in the vehicles_us dataset. It is a tool to simulate random events. It uses pandas, numpy, streamlit, and plotly.express. """
+# Carapp by Joseph Crea
+*This application explores different variables in the vehicles_us dataset.
+It is a tool to analyze the car data by varied data points. It uses pandas, streamlit, 
+and plotly.* 
+
+## Requirements
+- [Pyenv](https://github.com/pyenv/pyenv)
+  - specifically python version 3.11.9
+    - to install via pyenv `pyenv install 3.11.9`
+- [Pipenv](https://github.com/pypa/pipenv)
+
+## Local development
+1. Create a virtual environment to run your app with the required python version
+   - `pipenv --python 3.11.9`
+2. Install dependencies
+   - `pipenv install`
+3. To use the newly created virtual environment
+   ```
+   pipenv shell
+   ```
+
+    **OR** *to simply run the app*
+
+   ```
+   pipenv run streamlit run app.py
+    ```
--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import plotly.express as px
 # Load the dataset
 df = pd.read_csv('vehicles_us.csv')
 # create a text header above the dataframe
-st.header('Data viewer') 
+st.header('Data viewer')
 # display the dataframe with streamlit
 st.dataframe(df)
 # Title and description
@@ -13,17 +13,25 @@ st.title('Original Data on Used Cars')
 
 #create age categorires
 df['age'] = 2024 - df['model_year']
+
+
 def age_category(x):
-    if x<5: return '5-10'
-    elif x>=5 and x<10: return '5-10'
-    elif x>=10 and x<20: return '10-20'
-    else: return '>20'
+    if x < 5:
+        return '5-10'
+    elif x >= 5 and x < 10:
+        return '5-10'
+    elif x >= 10 and x < 20:
+        return '10-20'
+    else:
+        return '>20'
+
+
 # age cat. column
 df['age_category'] = df['age'].apply(age_category)
 #create a checkbox to show new cars
-show_new_cars = st.checkbox('Include new cars from dealers') 
+show_new_cars = st.checkbox('Include new cars from dealers')
 if not show_new_cars:
-    df = df[df.condition!='new']
+    df = df[df.condition != 'new']
 
 # Fill missing values with 0 and convert to boolean
 df['is_4wd'] = df['is_4wd'].fillna(0).astype(bool)
@@ -34,12 +42,12 @@ df['manufacturer'] = df['model'].str.split().str[0]
 #create a drop selection for manufacturers
 manufacturer_choice = df['manufacturer'].unique()
 
-selected_manufacturer = st.selectbox('choose a manufacurer', manufacturer_choice) 
+selected_manufacturer = st.selectbox('choose a manufacurer', manufacturer_choice)
 
 min_year, max_year = int(df['model_year'].min()), int(df['model_year'].max())
 
-year_range = st.slider("Choose years", value=(1960, 2024), min_value = min_year, max_value = max_year)
-actual_range = list(range(year_range[0], year_range[1]+1))
+year_range = st.slider("Choose years", value=(1960, 2024), min_value=min_year, max_value=max_year)
+actual_range = list(range(year_range[0], year_range[1] + 1))
 
 df_filtered = df[(df.manufacturer == selected_manufacturer) & (df.model_year.isin(list(actual_range)))]
 
@@ -63,17 +71,16 @@ choice_for_hist = st.selectbox('Split for price distribution', list_for_hist)
 fig1 = px.histogram(df, x='price', color=choice_for_hist)
 
 fig1.update_layout(
-title="<b> Split of price by {}</b>".format(choice_for_hist))
+    title="<b> Split of price by {}</b>".format(choice_for_hist))
 st.plotly_chart(fig1)
 
-list_for_scatter = ['odometer','cylinders','condition']
+list_for_scatter = ['odometer', 'cylinders', 'condition']
 
-choice_for_scatter = st.selectbox('Price dependency on',list_for_scatter)
+choice_for_scatter = st.selectbox('Price dependency on', list_for_scatter)
 
-fig2 = px.scatter(df, x="price", y=choice_for_scatter, color ="age_category",hover_data=['model_year'])
+fig2 = px.scatter(df, x="price", y=choice_for_scatter, color="age_category", hover_data=['model_year'])
 fig2.update_layout(title="<b> Price vs {}</b>".format(choice_for_scatter))
 st.plotly_chart(fig2)
-
 
 # Group by manufacturer and type to get the counts
 manufacturer_type_counts = df.groupby(['manufacturer', 'type']).size().unstack(fill_value=0)
@@ -81,11 +88,13 @@ manufacturer_type_counts = df.groupby(['manufacturer', 'type']).size().unstack(f
 manufacturer_type_counts = manufacturer_type_counts.reset_index()
 
 # Melt the dataframe to create a long format suitable for Plotly Express
-manufacturer_type_counts_melted = pd.melt(manufacturer_type_counts, id_vars=['manufacturer'], var_name='type', value_name='count')
+manufacturer_type_counts_melted = pd.melt(manufacturer_type_counts, id_vars=['manufacturer'], var_name='type',
+                                          value_name='count')
 
 # Plot using Plotly Express
 fig = px.bar(manufacturer_type_counts_melted, x='manufacturer', y='count', color='type', barmode='stack',
-             title='Vehicle Types by Manufacturer', labels={'manufacturer': 'Manufacturer', 'count': 'Count', 'type': 'Vehicle Type'})
+             title='Vehicle Types by Manufacturer',
+             labels={'manufacturer': 'Manufacturer', 'count': 'Count', 'type': 'Vehicle Type'})
 
 # Customize layout
 fig.update_layout(
